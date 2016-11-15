123a8691e37df41d1b8ee28e958d3bf82499a3bb
Eric SEYCHAL <eric.seychal@gmail.com>
2016-11-15 16:38:20 +0100

    ADD Picasso Library

--- app/src/main/java/com/ericseychal/holdall/PicassoActivity.java
+++ app/src/main/java/com/ericseychal/holdall/PicassoActivity.java
@@ -2,6 +2,9 @@ package com.ericseychal.holdall;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class PicassoActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class PicassoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picasso);
+
+        String urlPicture = "http://img0.mxstatic.com/wallpapers/b962ef609d737c4668f7b3e433bf615f_large.jpeg";
+
+        ImageView imageView = (ImageView) findViewById(R.id.picture_1);
+        Picasso.with(this).load(urlPicture).into(imageView);
     }
 }
