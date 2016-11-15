092b8fbc6bb0aad38cfdf8a49b31e4be28a8bdc3
Eric SEYCHAL <eric.seychal@gmail.com>
2016-11-15 14:27:04 +0100

    ADD button Test_1

--- app/src/main/java/com/ericseychal/holdall/MainActivity.java
+++ app/src/main/java/com/ericseychal/holdall/MainActivity.java
@@ -1,8 +1,9 @@
 package com.ericseychal.holdall;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +12,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView textView = (TextView) findViewById(R.id.text);
-        textView.setText("Bonjour");
+        Button button1 = (Button) findViewById(R.id.test_1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
     }
 }
