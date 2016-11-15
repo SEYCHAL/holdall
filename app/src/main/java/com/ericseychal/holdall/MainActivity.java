f13569f68a4b97d664d8c524b222c0800f77c876
Eric SEYCHAL <eric.seychal@gmail.com>
2016-11-15 15:57:31 +0100

    ADD PicassoMain and attached to MainActivity.Button_2

--- app/src/main/java/com/ericseychal/holdall/MainActivity.java
+++ app/src/main/java/com/ericseychal/holdall/MainActivity.java
@@ -1,5 +1,6 @@
 package com.ericseychal.holdall;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -18,6 +19,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "J'ai bien clické", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button button2 = (Button) findViewById(R.id.test_2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PicassoActivity.class);
+                startActivity(intent);
             }
         });
     }
