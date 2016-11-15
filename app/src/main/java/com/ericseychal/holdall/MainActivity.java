1dbf746770eb3974371ec1813c00a5d2f4eeebf5
Eric SEYCHAL <eric.seychal@gmail.com>
2016-11-15 14:41:08 +0100

    UPDATE Toast on button Test_1
    Modified button1 and add Toast

--- app/src/main/java/com/ericseychal/holdall/MainActivity.java
+++ app/src/main/java/com/ericseychal/holdall/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Toast.makeText(MainActivity.this, "J'ai bien clické", Toast.LENGTH_SHORT).show();
             }
         });
     }
