484056aaef7e0e3683e4a79ac180b2233bf32974
Eric SEYCHAL <eric.seychal@gmail.com>
2016-11-28 22:02:43 +0100

    ADD CallBAck on activity

--- app/src/main/java/com/ericseychal/holdall/listpicture/AdapterListPicture.java
+++ app/src/main/java/com/ericseychal/holdall/listpicture/AdapterListPicture.java
@@ -15,6 +15,9 @@ import com.squareup.picasso.Picasso;
 import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.OnClick;
+
 /**
  * Created by ericseychal on 22/11/2016.
  */
@@ -22,6 +25,11 @@ import java.util.List;
 public class AdapterListPicture extends BaseAdapter {
     private Context context;
     List<Pictures> picturesList = new ArrayList<>();
+
+    @BindView (R.id.list_picture_title) TextView title;
+    @BindView (R.id.list_picture_url) TextView url;
+    @BindView (R.id.list_picture_picture_1) ImageView imageView;
+    @BindView (R.id.FAB) FloatingActionButton fab;
 
     public AdapterListPicture(Context context) {
         this.context = context;
@@ -53,35 +61,27 @@ public class AdapterListPicture extends BaseAdapter {
     }
 
     @Override
+    @OnClick(R.id.FAB)
     public View getView(final int position, View convertView, ViewGroup parent) {
         if (convertView == null) {
             convertView = LayoutInflater.from(context).inflate(R.layout.list_picture_row,parent,false);
         }
-        TextView title = (TextView) convertView.findViewById(R.id.list_picture_title);
-        TextView url = (TextView) convertView.findViewById(R.id.list_picture_url);
-        ImageView imageView = (ImageView) convertView.findViewById(R.id.list_picture_picture_1);
+
         title.setText(picturesList.get(position).getName());
         url.setText(picturesList.get(position).getUrl());
         Picasso.with(context).load(picturesList.get(position).getUrl()).resize(200,200).into(imageView);
-/*
-        Button button = (Button) convertView.findViewById(R.id.list_picture_fab);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                pictureDel(position);
-            }
 
-        });
-        */
-        FloatingActionButton fab = (FloatingActionButton) convertView.findViewById(R.id.FAB);
         fab.setFocusable(false);
         fab.setFocusableInTouchMode(false);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                pictureDel(position);
-            }
-        });
+
+
+
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                pictureDel(position);
+//            }
+//        });
 
         return convertView;
     }
